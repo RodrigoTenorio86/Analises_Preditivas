17fefaf972388cf8c8b217ffde53abf8596fbb93
Rodrigo Tenório de Boa Ventura <boaventura19@yahoo.com.br>
2020-03-22 17:35:43 -0300

    Update regressao_logistica.R
    termino da taividade.

--- regressao_logistica.R
+++ regressao_logistica.R
@@ -14,25 +14,49 @@ library(ResourceSelection)
 
 names(credito2)
 
-
-
+table(sexo)
+table(estado_civil)
 table(risco_credito)
 table(risco)
 table(emprestimos)
 table(hipoteca)
 298+270+148
 
-
+credito2$sexo_masc<-ifelse(credito2$sexo=="masculino",1,0)
+credito2$conjuge<-ifelse(credito2$estado_civil=="casado",1,0)
 credito2$novo_emprestimos <- ifelse(credito2$emprestimos >= 1,1,0)
+credito2$hipoteca_existe <-  ifelse(credito2$hipoteca == "nao",0,1)
 
+table(credito2$conjuge)
 table(credito2$novo_emprestimos)
+table(credito2$hipoteca_existe)
 
 #credito2$novo_emprestimos<-as.factor(credito2$novo_emprestimos)
-table(credito2$novo_emprestimos)
+#table(credito2$novo_emprestimos)
 #credito2$novo_emprestimos<-relevel(credito2$novo_emprestimos,ref = 1) 
 
-futuro_emprestimos<-glm(credito2$novo_emprestimos ~ idade+salario+sexo+estado_civil+n_filhos+n_cartoes+hipoteca+emprestimos, data = credito2,family = binomial)
+
+plot(credito2$salario,credito2$novo_emprestimos,pch=20 )
+plot(credito2$n_filhos,credito2$novo_emprestimos,pch=20  )
+plot(credito2$idade,credito2$novo_emprestimos,pch=20)
+cor(credito2$idade,credito2$novo_emprestimos)
+cor(credito2$salario,credito2$novo_emprestimos)
+cor(credito2$emprestimos,credito2$novo_emprestimos )
+cor(credito2$n_filhos,credito2$novo_emprestimos)
+cor(credito2$idade,credito2$novo_emprestimos )
+cor(credito2$n_cartoes,credito2$novo_emprestimos)
+cor(credito2$hipoteca_existe,credito2$novo_emprestimos)
+cor(credito2$salario,credito2$novo_emprestimos)
+cor(credito2$risco_credito,credito2$novo_emprestimos)
+cor(credito2$sexo_masc,credito2$novo_emprestimos)
+
+
+futuro_emprestimos<-glm(credito2$novo_emprestimos ~ idade+salario+n_filhos+n_cartoes+salario+risco_credito+hipoteca_existe+sexo+conjuge, data = credito2,family = binomial)
 
 
 summary(futuro_emprestimos)
 exp(futuro_emprestimos$coefficients)
+
+#A variavel probabilidade_novo_emprestimo, é a Probabilidade para tomada de novos emprestimos.
+credito2$probabilidade_novo_emprestimo= predict(futuro_emprestimos,type = "response", newdata = credito2)
+View(credito2)
