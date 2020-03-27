6d2a816ae0b533d891115aaed7e5bbf3e4603ff8
Rodrigo Tenório de Boa Ventura <boaventura19@yahoo.com.br>
2020-03-26 18:31:54 -0700

    Update regressao_logistica.R
    Atualizacao_26_03_2020

--- regressao_logistica.R
+++ regressao_logistica.R
@@ -1,39 +1,62 @@
-#Com base nesse arquivo, vamos criar um modelo de Regressão Linear para estimar o salário dos futuros empregados da empresa XBTO Ltda.
 library(readxl)
-Regressao_Linear_1_ <- read_excel("Downloads/Regressao_Linear.xlsx")
-View(Regressao_Linear_1_)
+credito2 <- read_excel("Downloads/credito2.xlsx")
 
-attach(Regressao_Linear_1_)
-names(Regressao_Linear_1_)
-#OBS: excluir variavel salnow
-Regressao_Linear_1_$sex_males<-ifelse(Regressao_Linear_1_$sex=="Males",1,0)
-plot(Regressao_Linear_1_$sex_males,Regressao_Linear_1_$salbeg)
-plot(Regressao_Linear_1_$time,Regressao_Linear_1_$salbeg)
-plot(Regressao_Linear_1_$age,Regressao_Linear_1_$salbeg)
-plot(Regressao_Linear_1_$edlevel,Regressao_Linear_1_$salbeg)
-plot(Regressao_Linear_1_$work,Regressao_Linear_1_$salbeg)
-
-cor(Regressao_Linear_1_$sex_males,Regressao_Linear_1_$salbeg)
-cor(Regressao_Linear_1_$time,Regressao_Linear_1_$salbeg)
-cor(Regressao_Linear_1_$age,Regressao_Linear_1_$salbeg)
-cor(Regressao_Linear_1_$edlevel,Regressao_Linear_1_$salbeg)
-cor(Regressao_Linear_1_$work,Regressao_Linear_1_$salbeg)
-
-cor.test(Regressao_Linear_1_$sex_males,Regressao_Linear_1_$salbeg)
-cor.test(Regressao_Linear_1_$time,Regressao_Linear_1_$salbeg)
-cor.test(Regressao_Linear_1_$age,Regressao_Linear_1_$salbeg)
-cor.test(Regressao_Linear_1_$edlevel,Regressao_Linear_1_$salbeg)
-cor.test(Regressao_Linear_1_$work,Regressao_Linear_1_$salbeg)
+View(credito2)
 
 
-boxplot(Regressao_Linear_1_$salbeg)
+attach(credito2)
 
-salario_futuro_func=lm(Regressao_Linear_1_$salbeg~edlevel+time+age+work+sex,data = Regressao_Linear_1_)
-salario_futuro_func
-#salario=b0+b1*edlevel+b2*time+b3*age+b4*work+b5*sex
-#salario=-2525.81+652.74*edlevel+(-22.48*time)+36.75*age+16.14*work+1567.93*sex
-#salario_futuro_func[[1]]+salario_futuro_func[[2]]*Regressao_Linear_1_$edlevel
-plot(Regressao_Linear_1_$edlevel,Regressao_Linear_1_$salbeg, main = "Diagrama da Reta")
-abline(salario_futuro_func,col="red")
-summary(salario_futuro_func)
+install.packages(c("glmnet","glm2","ROCR","ResourceSelection"))
+library(glmnet)
+library(glm2)                 
+library(ROCR)
+library(ResourceSelection)
 
+names(credito2)
+
+table(sexo)
+table(estado_civil)
+table(risco_credito)
+table(risco)
+table(emprestimos)
+table(hipoteca)
+298+270+148
+
+credito2$sexo_masc<-ifelse(credito2$sexo=="masculino",1,0)
+credito2$conjuge<-ifelse(credito2$estado_civil=="casado",1,0)
+credito2$novo_emprestimos <- ifelse(credito2$emprestimos >= 1,1,0)
+credito2$hipoteca_existe <-  ifelse(credito2$hipoteca == "nao",0,1)
+
+table(credito2$conjuge)
+table(credito2$novo_emprestimos)
+table(credito2$hipoteca_existe)
+
+#credito2$novo_emprestimos<-as.factor(credito2$novo_emprestimos)
+#table(credito2$novo_emprestimos)
+#credito2$novo_emprestimos<-relevel(credito2$novo_emprestimos,ref = 1) 
+
+
+plot(credito2$salario,credito2$novo_emprestimos,pch=20 )
+plot(credito2$n_filhos,credito2$novo_emprestimos,pch=20  )
+plot(credito2$idade,credito2$novo_emprestimos,pch=20)
+cor(credito2$idade,credito2$novo_emprestimos)
+cor(credito2$salario,credito2$novo_emprestimos)
+cor(credito2$emprestimos,credito2$novo_emprestimos )
+cor(credito2$n_filhos,credito2$novo_emprestimos)
+cor(credito2$idade,credito2$novo_emprestimos )
+cor(credito2$n_cartoes,credito2$novo_emprestimos)
+cor(credito2$hipoteca_existe,credito2$novo_emprestimos)
+cor(credito2$salario,credito2$novo_emprestimos)
+cor(credito2$risco_credito,credito2$novo_emprestimos)
+cor(credito2$sexo_masc,credito2$novo_emprestimos)
+
+
+futuro_emprestimos<-glm(credito2$novo_emprestimos ~ idade+salario+n_filhos+n_cartoes+salario+risco_credito+hipoteca_existe+sexo+conjuge, data = credito2,family = binomial)
+
+
+summary(futuro_emprestimos)
+exp(futuro_emprestimos$coefficients)
+
+#A variavel probabilidade_novo_emprestimo, é a Probabilidade para tomada de novos emprestimos.
+credito2$probabilidade_novo_emprestimo= predict(futuro_emprestimos,type = "response", newdata = credito2)
+View(credito2)
