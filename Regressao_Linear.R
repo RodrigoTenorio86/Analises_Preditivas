26b887f5a1c6438c0a388023e283471cbf3ba0a5
Rodrigo Tenório de Boa Ventura <boaventura19@yahoo.com.br>
2020-03-27 18:56:55 -0700

    Update Regressao_Linear.R
    Finalizacao V1

--- Regressao_Linear.R
+++ Regressao_Linear.R
@@ -1,4 +1,6 @@
 install.packages("car")
+install.packages("nortest")
+library(nortest)
 library(car)
 library(readxl)
 library(faraway)
@@ -22,7 +24,7 @@ salario_futuro_func
 
 #Interpretacao dos coeficiente associado ao efeito das variaveis(edlevel, time, age, work e sexMales)
 #, no salário dos futuros empregados da empresa XBTO Ltda.
-#Considerado os dados e o modelo das variaveis(edlevel, time, age, work e sex) para construir estes modelos abaixo:
+#Considerado os dados e o modelo das variaveis(edlevel, time, age, work e sex) para construir estes modelos equacoes abaixo:
 
 #salario=b0+b1*edlevel+b2*time+b3*age+b4*work+b5*sex
 #salario=-2525.81+652.74*edlevel+(-22.48*time)+36.75*age+16.14*work+1567.93*sex
@@ -40,12 +42,9 @@ Sexo = factor(Regressao_Linear_1_$sex_males)
 par(mfrow=c(1,1))
 plot(Regressao_Linear_1_$work[Sexo==1],Regressao_Linear_1_$salbeg[Sexo==1],xlab="Experiência na função",ylab="Salário")
 points(Regressao_Linear_1_$work[Sexo==0],Regressao_Linear_1_$salbeg[Sexo==0], pch = 9)
-#Com este grafico de dispers'ao de Salario versus Experiencia 
+#Com este grafico de dispersao de Salario versus Experiencia, mostrar diferenca de salario entre homens e mulheres. 
 plot(Regressao_Linear_1_$sex_males,Regressao_Linear_1_$salbeg)
-plot(Regressao_Linear_1_$time,Regressao_Linear_1_$salbeg)
-plot(Regressao_Linear_1_$age,Regressao_Linear_1_$salbeg)
-plot(Regressao_Linear_1_$edlevel,Regressao_Linear_1_$salbeg)
-plot(Regressao_Linear_1_$work,Regressao_Linear_1_$salbeg)
+#Neste grafico Mostra que existem mais homens com salario superior 15000 em comparacao com as mulheres. 
 
 cor(Regressao_Linear_1_$sex_males,Regressao_Linear_1_$salbeg)
 cor(Regressao_Linear_1_$time,Regressao_Linear_1_$salbeg)
@@ -62,11 +61,11 @@ homens_salarios <- Regressao_Linear_1_$salbeg[Regressao_Linear_1_$sex_males==1]
 mulheres_salarios<-Regressao_Linear_1_$salbeg[Regressao_Linear_1_$sex_males==0]
 
 mean(homens_salarios)-mean(mulheres_salarios)
-# esta media mostra que sao os homens que granhao mais de R$2883. do salario medio.
+# esta media mostra que sao os homens que granhao mais de 2883. do salario medio.
 mean(homens_salarios)
 mean(mulheres_salarios)
 t.test(homens_salarios ,mulheres_salarios)
-#com valor p-value < 2.2e-16 significa que media de salario entre homens e mulheres sao muito grandes.
+#com Teste t e seu valor p-value < 2.2e-16 significa que media de salario entre homens e mulheres sao muito grandes.
 
 
 #2)   Faça uma correlação linear entre as variáveis:
@@ -81,6 +80,10 @@ cor.test(Regressao_Linear_1_$work,Regressao_Linear_1_$salbeg)
 
 modelo <- Regressao_Linear_1_[,c("id","salbeg","time","age","edlevel","work")] 
 head(modelo)
+
+# As variáveis que possuem as maiores correlação com salbeg sao edlevel e sex_males.
+#ambas variaveis sao positival e são estatisticamente significativas.
+
 
 #2.2 Realiza um regressão linear múltipla com todas as variáveis relevantes para a equação e, 
 #qual o valor do  Justifique porquê as variáveis que entraram na equação é relevantes? 
@@ -101,7 +104,7 @@ summary(salario_futuro_func)$r.squared
 summary(salario_futuro_func)
 vif(salario_futuro_func)
 cbind(vif(salario_futuro_func))
-# Apos analise com a funcao vif e visto que nao existem valores maiores que 10 , logo nao existem presenca de multicolinearidade.
+# Apos analise com a funcao vif e visto que nao existem valores maiores que 10, logo nao existem presenca de multicolinearidade.
 
 #cook<-influence.measures(salario_futuro_func)
 #plot(lm(salario_futuro_func),which = 4)
@@ -110,8 +113,22 @@ cbind(vif(salario_futuro_func))
 #2.5 Faça uma análise dos resíduos (gráficos e normalidade do teste) do modelo final.
 salario_futuro_func=lm(Regressao_Linear_1_$salbeg~edlevel+time+age+work+sex_males,data = Regressao_Linear_1_)
 salario_futuro_func
+residuos<-residuals(salario_futuro_func)
+ajuste<-fitted(salario_futuro_func)
 
+Regressao_Linear_1_$ajuste<-ajuste
+Regressao_Linear_1_$residuos<-residuos
+head(Regressao_Linear_1_)
 
+mean(residuos)
+median(residuos)
+boxplot(residuos)
+plot(residuos)
+hist(residuos)
+qqnorm(residuos)
+qqline(residuos)
+
+lillie.test(residuos)
 
 #2.6 O modelo/ equação está adequada?
 salario_futuro_func=lm(Regressao_Linear_1_$salbeg~edlevel+time+age+work+sex_males,data = Regressao_Linear_1_)
@@ -121,10 +138,23 @@ summary(salario_futuro_func)$r.squared
 
 #2.7 Faça um previsão de salário para um candidato masculino/ feminino. 
 #Precisar conter na formula todas as variáveis relevantes.
-populacao_masc<-Regressao_Linear_1_[,c()]
-previsao_masculina<-predict(salario_futuro_func,)
 
+#separacao por sexo
+populacao_masculina<-Regressao_Linear_1_[Regressao_Linear_1_$sex_males==0,]
+View(populacao_masculina)
+populacao_feminino<-Regressao_Linear_1_[Regressao_Linear_1_$sex_males==1,]
+View(populacao_feminino)
+#possivel problema com outliers
+salario_futuro_func_masc=lm(populacao_masculina$salbeg~edlevel+time+age+work,data = populacao_masculina)
+summary(salario_futuro_func_masc)
 
+salario_futuro_func_femi<-lm(populacao_feminino$salbeg~edlevel+time+age+work,data = populacao_feminino)
+summary(salario_futuro_func_femi)
 
+previsao_masc<-predict(salario_futuro_func_masc,populacao_masculina)
+populacao_masculina$previsao_masc<-previsao_masc
+head(populacao_masculina)
 
-
+previsao_femi<-predict(salario_futuro_func_femi,populacao_feminino)
+populacao_feminino$previsao_femi<-previsao_femi
+head(populacao_feminino)
